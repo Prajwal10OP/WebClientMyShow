b9a6b1019ef363e89f5980b8262e5dff35ef3316
Prajwal10OP <78852644+Prajwal10OP@users.noreply.github.com>
2023-03-10 11:23:40 +0530

    Add files via upload

---
+++ MyShow-WebClient/src/test/java/com/example/webclient/MyShowWebClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.webclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyShowWebClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+		
+		
+	}
+
+}
